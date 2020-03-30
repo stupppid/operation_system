a3e80e94c2d3c4d96869592a711bea06a72608c8
jsmorty <1538026893@qq.com>
2020-03-30 19:44:36 +0800

    修复几个bug

--- src/index.js
+++ src/index.js
@@ -2,15 +2,9 @@ import {init} from "./core";
 import {FileController} from "./core/controller";
 import {AccountController} from "./core/controller";
 import states from "./core/controller/state";
-/**
- * 1. 初始化数据库  indexdb作为磁盘  storage作为简单缓存
- * 2. 获取配置数据      theme language account等config
- * 3. 使用配置数据运行  走到login
- */
-document.addEventListener("DOMContentLoaded", function(event) {
-    init().then()
-});
 
 export const state = states
 export const fileController = new FileController()
 export const accountController = new AccountController()
+export const initialize = init
+
