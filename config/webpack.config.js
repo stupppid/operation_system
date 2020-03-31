9d06dc5ada4523a137c2d13c2c1ae66108d8305a
jsmorty <1538026893@qq.com>
2020-03-31 19:15:15 +0800

    替换md5为sha256

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -22,10 +22,7 @@ module.exports = {
                 test: /\.ts$/,
                 exclude: /node_modules/,
                 use: [
-
                     {
-                        loader: 'babel-loader',
-                    },{
                         loader: 'ts-loader'
                     }
                 ]
